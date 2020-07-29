f5629145489cff7443db92962568e15671917ce0
sounmind <37020415+sounmind@users.noreply.github.com>
2020-07-29 09:58:56 +0900

    미션3: 함수 인자로 data를 받아서 문자열 입력 가능, 예외 처리 완료, UI 개선
    1. 문자열 입력에 관하여
    저는 이미 선언해 놓은 배열의 주소값을 data로 queueArray에 차례로 집어넣고 있었어요. 제가 입력할 때마다 그 값이 이미 주소값이 고정된 inputContents 아래에 입력됐습니다. 따라서 queueArray에 처음부터 입력된 주소값들은 모두 같은 주소값들이었고 따라서 inputContents가 입력될 때마다 모든 queueArray의 요소들이 그 값으로 바뀌었던 것으로 생각됩니다!
    문제: 이미 생성된 배열의 내용을 아무리 바꿔도 똑같은 주소값이 전달됐던 것으로 생각됨.
    이전의 잘못된 코드에서 배열에 담겼다고 판단되는 예) char *queueArray[QUEUE_LENGTH] = { 0x1234, 0x1234, 0x1234, 0x1234 ... };  큐를 담는 포인터 배열에 똑같은 주소값만 입력됐다.
    해결: add 함수를 실행하기 전마다 새로운 배열을 malloc했다. 그것을  add 함수의 인자로 전달해, 새로운 주소값을 queueArray에 입력했다.
    아래는 이미 생성된 배열의 내용을 바꾸어도 그 이름이 가리키는 주소값은 변하지 않는다는 실험입니다. (새로 알았습니다...!)
    
    ```
    #include <stdio.h>
    int main(void)
    {   
        char charArray[100]; // 크기가 100인 문자 배열 생성
        printf("초기화 없이 배열 %%s로 출력: (쓰레기값이 들어 있음) %s\n", charArray); // 배열 읽기
        printf("배열 charArray에 담긴 주소값: %p\n\n", charArray); // 배열의 이름은 그 자체로 포인터인가?
        // 배열의 내용을 바꾸어도 배열 이름이 가리키는 주소값은 동일한가?
        printf("charArray에 문자열 입력: ");
        scanf("%s", charArray);
        printf("\n배열 %%s로 출력 %s\n", charArray);
        printf("배열 charArray에 담긴 주소값: %p\n", charArray);
    }
    /*  -----------------------출력 결과----------------------------
        초기화 없이 배열 %s로 출력: (쓰레기값이 들어 있음) 淆a  
        배열 charArray에 담긴 주소값: 0061FEBC
        charArray에 문자열 입력: abcdefg
        배열 %s로 출력 abcdefg
        배열 charArray에 담긴 주소값: 0061FEBC
    */
    ```
    
    2. 예외 처리에 관하여
    사실 1에서 4까지의 숫자만 받게 하고 나머지 모든 경우가 어떻게든 처리가 되기 때문에 이전의 방식 그대로 좋다고 판단했습니다. 이 프로그램에 한해서 예외처리의 목적에 부합하다고 볼 수 있다고 생각했습니다.

---
+++ sounmind/week3/w3-3-str.c
@@ -0,0 +1,162 @@
+/*
+제목: 문자열이 입력되는, 배열을 이용한 원형 큐 구현,
+
+(개선된) 원형 큐
+큐가 텅 비었을 때
+    front와 rear가 같은 위치를 가리킨다.
+큐에 데이터를 넣을 때
+    rear를 다음 위치로 이동시킨 뒤 그곳에 데이터를 저장한다.
+큐에서 데이터를 뺄 때
+    front를 다음 위치로 옮긴다음, 그 위치에 있는 값을 반환한다.
+큐가 꽉 찼을 때
+    rear가 가리키는 위치의 다음 위치를 front가 가리킨다.
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#define QUEUE_LENGTH 10 + 1 // 배열 길이가 N이라면, N-1개 데이터가 채워졌을 때 꽉찬 것으로 간주
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h> 
+
+typedef struct circularQueue // 원형 큐 자료형 구조체 선언
+{
+    int front; // 큐의 머리
+    int rear; // 큐의 꼬리
+    char *queueArray[QUEUE_LENGTH]; // 큐의 포인터(변수로 이뤄진) 배열
+} Queue;
+
+void queueInit(Queue* pointerQueue);
+int isQueueEmpty(Queue* pointerQueue);
+int queueNextPositionIndex(int position);
+void add(Queue* pointerQueue, char * data);
+void pop(Queue* pointerQueue);
+void display(Queue* pointerQueue);
+
+int main(void) {
+
+    Queue queue; // 큐 선언
+    queueInit(&queue); // 큐 초기화
+
+    char order[100];
+
+    while (1)
+    {
+        printf(">>>>>>>>>>>>>>>>>>>> 명령 번호를 입력하세요\n");
+        printf(">>>> add(1), pop(2), display(3), quit(4) : ");
+        scanf("%s", &order);
+
+        if (strlen(order) > 1) { printf("\n>>>> 명령이 너무 깁니다!\n\n"); continue; }                // 예외1) 입력 문자가 두 자리 이상이면 continue;
+        if (atoi(order) == 0) { printf("\n>>>> 명령이 해당 숫자(1~4)가 아닙니다!\n\n"); continue; }    // 예외2) 입력 문자가 숫자가 아니면 continue;
+
+        switch (atoi(order)) {
+        case 1: // add
+        {
+            char *inputContents = malloc(sizeof(char)*100);                                         // 큐에 입력할 배열을 위해 충분한 크기로 메모리 할당
+            
+            printf(">>>> 추가할 내용을 입력하세요 :            ");
+            scanf("%s", inputContents);
+
+            add(&queue, inputContents);
+            break;
+        }
+
+        case 2: // pop
+        {
+            pop(&queue);
+            break;
+        }
+        case 3: // display
+        {
+            display(&queue);
+            break;
+        }
+        case 4: // quit
+        {
+            printf("\n>>>> 프로그램을 종료합니다.\n");
+            return 0;
+        }
+        default: // 예외3) '한 자리'에다가 '숫자'이지만 해당 범위(1~4)가 아닌 경우
+        {
+            printf("\n>>>> 다시 제대로 명령 번호를 입력해주세요.\n\n");
+            break;
+        }
+        }
+    }
+    return 0;
+}
+
+
+
+void queueInit(Queue* pointerQueue)
+{
+    pointerQueue->front = 0;
+    pointerQueue->rear = 0;
+}
+
+int isQueueEmpty(Queue* pointerQueue)
+{
+    if (pointerQueue->front == pointerQueue->rear)                          // 큐가 텅 비었다 == front와 rear가 동일한 위치를 가리킨다
+    { return 1; }
+    else
+    { return 0; }
+}
+
+int queueNextPositionIndex(int position)
+{
+    if (position == QUEUE_LENGTH - 1)                                       // 위치가 배열의 마지막 요소의 인덱스 값이라면
+    { return 0; }
+    else
+    { return position + 1; }   
+}
+
+void add(Queue* pointerQueue, char * data)
+{
+    if (queueNextPositionIndex(pointerQueue->rear) == pointerQueue->front)  // 큐가 꽉 찼다면, (rear가 가리키는 곳의 다음 위치가 front가 가리키는 곳)
+    {
+        printf("\n큐가 꽉 찼습니다!\n\n");
+        return;
+    }
+    pointerQueue->rear = queueNextPositionIndex(pointerQueue->rear);        // rear를 한 칸 이동
+    pointerQueue->queueArray[pointerQueue->rear] = data;                    // rear가 가리키는 곳에 데이터 저장
+
+    printf("\n정상 입력되었습니다...\n\n");
+    return;
+}
+
+void pop(Queue* pointerQueue)
+{
+    if (isQueueEmpty(pointerQueue))
+    {
+        printf("\n큐가 텅 비었습니다!\n\n");
+        return;
+    }
+
+    pointerQueue->front = queueNextPositionIndex(pointerQueue->front);                                  // front를 한 칸 이동
+    printf("\n>>>>>>>> Pop Executed: \" %s \"\n\n", pointerQueue->queueArray[pointerQueue->front]);     // front가 가리키는 데이터 반환
+
+}
+
+void display(Queue* pointerQueue)
+{
+    if (isQueueEmpty(pointerQueue))
+    {
+        printf("\n큐가 텅 비었습니다!\n\n");
+        return;
+    }
+
+    printf("\n>>>>>>>> Queue Start <<<<<<<<\n\n");
+    int i = pointerQueue->front;
+
+    while (1)
+    {
+        i = queueNextPositionIndex(i); // 다음 위치로 이동
+
+        printf("index= %d, value= %s \n", i, pointerQueue->queueArray[i]); // 해당 위치에 있는 값 출력
+
+        if (pointerQueue->rear == i) // 해당 위치가 rear가 가리키는 위치면 함수 종료
+        {
+            printf("\n>>>>>>>> Queue  End  <<<<<<<<\n\n");
+            return;
+        }
+    }
+}
