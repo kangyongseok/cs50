f3b02efce4753b9fe3b8a3b6a651fa5d72bf4b30
강용석 <kangyongsuek@gmail.com>
2020-08-10 21:44:12 +0900

    add: memory control func 간단하게 사용해보기

--- think_dev/5주차미션/mission2.c
+++ think_dev/5주차미션/mission2.c
@@ -1,24 +1,54 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(void) {
-    char str1[13] = "Hello, world";
-    char str2[6] = "abcde";
+char str1[13] = "Hello, world";
+char str2[6] = "abcde";
 
-    // strcpy(str1, str2);
-    // printf("%s\n", str1); // abcde
-    // int length = sizeof(str1) / sizeof(char);
-    // for (int i = 0; i < length; i++) {
-    //     printf("%c", str1[i]); // abcde world
-    // }
+void strncpyPlay(void);
+void strcpyPlay(void);
+void memsetPLay(void);
+void memmovePlay(void);
+
+int main(void) {
+
+    strcpyPlay();
+    strncpyPlay();
+    memsetPLay();
+    memmovePlay();
+}
+
+void strncpyPlay(void) {
     strncpy(str1, str2, sizeof(str1) - 1);
     printf("%s\n", str1);
-    // int length = sizeof(str1) / sizeof(char);
     for (int i = 0; i < 13; i++) {
         printf("%c", str1[i]);
     }
+    printf("\n");
+}
 
-    memset(str1, 'a', 13 * sizeof(char));
-    for (int i = 0; i < 13; i++) {
-        printf("%c\n", str1[i]);
+void strcpyPlay(void) {
+    strcpy(str1, str2);
+    printf("%s\n", str1); // abcde
+    int length = sizeof(str1) / sizeof(char);
+    for (int i = 0; i < length; i++) {
+        printf("%c", str1[i]); // abcde world
     }
+    printf("\n");
+}
+
+void memsetPLay(void) {
+     memset(str1, 'a', 13 * sizeof(char));
+    for (int i = 0; i < 13; i++) {
+        printf("%c", str1[i]);
+    }
+    printf("\n");
+}
+
+void memmovePlay(void) {
+    int arr[5] = {1, 2, 3, 4, 5};
+    memmove(arr, arr + 1, sizeof(int) * 3);
+    for (int i = 0; i < 5; i++) {
+        printf("%d", arr[i]);
+    }
+    printf("\n");
+}
